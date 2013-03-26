54ea29d293e87bc8ec16d7aa11e8e50df8ad33bc
alesblaznik <ales@blaznik.si>
2013-03-26 22:18:43 +0100

    Dodani osnovni modeli.

---
+++ OpenCollea/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+class UserProfile(User):
+    timezone = models.CharField(max_length=40, default='Europe/Ljubljana')
+    language_code = models.CharField(max_length=5, default='en-us')
+    avatar = models.ImageField()
+    courses_enrolled = models.ManyToManyField(Course)
+    website = models.URLField()
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    machine_readable_title = models.SlugField(max_length=50)
+    description = models.TextField()
+    website = models.URLField()
+
+    def save(self, *args, **kwargs):
+        if not self.id and not self.machine_readable_title:
+            # Title to machine readable format
+            self.machine_readable_title = slugify(self.title)
+
+        super(Course, self).save(*args, **kwargs)
+
+class Reference(models.Model):
+    title = models.CharField(max_length=50)
+    course = models.ForeignKey(Course)
+    user = models.ForeignKey(UserProfile)
+    author = models.CharField(max_length=50)
+    type = models.ForeignKey(ReferenceType)
+    attachments = models.ManyToManyField(Attachment)
+    tags = models.ManyToManyField(Tag)
+    abstract = models.TextField()
+    note = models.TextField()
+    published = models.DateField()
+    link = models.URLField()
+
+class ReferenceType(models.Model):
+    title = models.CharField(max_length=20)
+    machine_readable_title = models.CharField(max_length=50, unique=True)
+
+class Attachment(models.Model):
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(UserProfile)
+    file = models.FileField()
+    mime_type = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=20)
+    machine_readable_title = models.SlugField(max_length=50, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id and self.title and not self.machine_readable_title:
+            # Title to machine readable format
+            self.machine_readable_title = slugify(self.title)
+
+        super(Tag, self).save(args, kwargs)
+
+class Question(models.Model):
+    user = models.ForeignKey(UserProfile)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    user = models.ForeignKey(UserProfile)
+    content = models.TextField()
