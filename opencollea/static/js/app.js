694bebf9778e2d4d0e83fa9d76098886f9708752
Ziga Emersic <ziga.emersic@gmail.com>
2013-04-28 19:33:01 +0200

    Dodajanje Coursea
    Dodajanje Coursea (Classa)

--- opencollea/static/js/app.js
+++ opencollea/static/js/app.js
@@ -5,5 +5,6 @@ angular.module('opencollea', ['openColleaServices','http-auth-interceptor']).
             .when('/', {templateUrl: '/templates/base.html',   controller: OpenColleaCtrl})
             .when('/courses', {templateUrl: '/templates/courses.html',   controller: OpenColleaCtrl})
             .when('/profile/:username', {templateUrl: '/static/partials/profile/user-profile-class.html'})
+            .when('/new-course', {templateUrl: '/static/partials/course/new-course.html', controller: CourseCtrl})
             .otherwise({redirectTo: '/'});
     }]);
--- opencollea/static/js/controllers.js
+++ opencollea/static/js/controllers.js
@@ -31,3 +31,40 @@ angular.module('opencollea').controller({
 function OpenColleaCtrl($scope, Course) {
     $scope.courses = Course.query();
 }
+
+function CourseCtrl($scope, $http) {
+    $scope.newCourse = function() {
+        data = {
+          'title': $scope.title,
+          'machine_readable_title': $scope.machine_readable_title,
+          'description': $scope.description,
+          'website': $scope.website
+        }
+        var formEl = $('#newCourseForm');
+        var msgDiv = $(formEl).find('.msgDiv:eq(0)');
+
+        // reset:
+        $(formEl).find('input[type="text"]').each(function(i,e){
+            $(e).removeClass('requiredFieldError');
+        });
+        $(msgDiv).hide().removeClass('alert alert-error alert-success').text('');
+
+        $http.post('/api/v1/course/new', data).success(function(response) {
+            if (true == response.success) {
+                $(msgDiv).addClass('alert-success').text('Course inserted');
+                $(formEl).find('input[type="text"]').each(function(i,e){
+                    $(e).val('');
+                });
+            } else if (response.required) {
+                for (var i = 0; i < response.required.length; i++){
+                    $('input[ng-model="' + response.required[i] + '"]').addClass('requiredFieldError');
+                }
+                $(msgDiv).addClass('alert').text('Please, fill in required fields');
+            } else {
+                $(msgDiv).addClass('alert-error').text('Error creating new course');
+                console.warn(response);
+            }
+            $(msgDiv).show();
+        });
+    }
+}
