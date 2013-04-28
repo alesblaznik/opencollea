163615872f0cc1ec107173edefade9a7bee4745b
alesblaznik <ales@blaznik.si>
2013-04-28 18:02:51 +0200

    Umestitev UserProfile plain HTML.

--- opencollea/static/js/app.js
+++ opencollea/static/js/app.js
@@ -2,8 +2,10 @@
 angular.module('opencollea', ['openColleaServices','http-auth-interceptor']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/', {templateUrl: '/templates/base.html',   controller: OpenColleaCtrl})
-            .when('/courses', {templateUrl: '/templates/courses.html',   controller: OpenColleaCtrl})
-            .when('/profile/:username', {templateUrl: '/static/partials/profile/user-profile-class.html'})
+            .when('/', {controller: OpenColleaCtrl})
+            .when('/courses', {templateUrl: '/templates/courses.html', controller: OpenColleaCtrl})
+            .when('/profile/:username', {templateUrl: '/static/partials/profile/user-profile-class.html' })
+            .when('/profile/:username/edit', {templateUrl: '/static/partials/profile/user-profile-form.html'})
+            .when('/auth/:username/edit', {templateUrl: '/static/partials/profile/user-registration-edit-form.html'})
             .otherwise({redirectTo: '/'});
     }]);
