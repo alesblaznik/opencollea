3931e2fe9eaa68df5e80379bc50b06928731a18c
alesblaznik <ales@blaznik.si>
2013-05-14 14:07:38 +0200

    Only GET methods allowed on code_register's resources.

--- code_register/resources.py
+++ code_register/resources.py
@@ -7,27 +7,32 @@ class GenderResource(ModelResource):
     class Meta:
         queryset = Gender.objects.all()
         resource_name = 'gender'
+        allowed_methods = ['get']
 
 
 class LanguageResource(ModelResource):
     class Meta:
         queryset = Language.objects.all()
         resource_name = 'language'
+        allowed_methods = ['get']
 
 
 class AgeRangeResource(ModelResource):
     class Meta:
         queryset = AgeRange.objects.all()
         resource_name = 'age_range'
+        allowed_methods = ['get']
 
 
 class OccupationResource(ModelResource):
     class Meta:
         queryset = Occupation.objects.all()
         resource_name = 'occupation'
+        allowed_methods = ['get']
 
 
 class AreaOfStudyResource(ModelResource):
     class Meta:
         queryset = AreaOfStudy.objects.all()
         resource_name = 'area_of_study'
+        allowed_methods = ['get']
