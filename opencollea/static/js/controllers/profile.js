6d979120210af4f7dec1a55f1b9bcb980c7b0d6a
alesblaznik <ales@blaznik.si>
2013-05-15 21:21:44 +0200

    Added notifications.

--- opencollea/static/js/controllers/profile.js
+++ opencollea/static/js/controllers/profile.js
@@ -1,5 +1,7 @@
-app.controller('UserProfileEditCtrl', ['$scope', 'UserProfile', 'Gender', 'Language', 'AgeRange', 'Occupation', 'AreaOfStudy',
-    function($scope, UserProfile, Gender, Language, AgeRange, Occupation, AreaOfStudy) {
+app.controller('UserProfileEditCtrl', ['$scope', '$rootScope', 'UserProfile', 'Gender', 'Language', 'AgeRange', 'Occupation', 'AreaOfStudy',
+    function($scope, $rootScope, UserProfile, Gender, Language, AgeRange, Occupation, AreaOfStudy) {
+        $scope.errors = { user_profile: null };
+
         // Load UserProfile
         $scope.user_profile = UserProfile.get({userId: $scope.currentUser.id});
 
@@ -12,8 +14,28 @@ app.controller('UserProfileEditCtrl', ['$scope', 'UserProfile', 'Gender', 'Langu
 
         $scope.saveProfile = function() {
             $scope.loading = true;
-            $scope.user_profile.$update({userId: $scope.currentUser.id}, function() {
-                $scope.loading = false;
-            });
+            $scope.user_profile.$update({userId: $scope.currentUser.id},
+                // Success
+                function() {
+                    $scope.loading = false;
+
+                    $rootScope.notifications = [{
+                      class: 'alert-success',
+                      content: 'Your profile was successfuly saved.'
+                    }];
+                    window.scrollTo(0,0);
+                },
+                // Failed
+                function(response) {
+                    $scope.loading = false;
+                    $scope.errors.user_profile = response.data.user_profile;
+
+                    $rootScope.notifications = [{
+                      class: 'alert-error',
+                      content: "We're sorry, but please check again those pretty red labels."
+                    }];
+                    window.scrollTo(0,0);
+                }
+            );
         }
 }]);
