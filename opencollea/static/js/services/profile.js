1510c1eb39c10b0f5d4a4ebacf4be2d1c44156e6
alesblaznik <ales@blaznik.si>
2013-05-16 11:51:44 +0200

    Custom UserProfile filtering.

--- opencollea/static/js/services/profile.js
+++ opencollea/static/js/services/profile.js
@@ -1,5 +1,5 @@
 app.factory('UserProfile', ['$resource', function($resource) {
-    return $resource('/api/v1/user_profile/:userId?username=:username',
+    return $resource('/api/v1/user_profile/:userId?:filter',
         {},
         {'update':   {method:'PUT'}});
 }]);
