033d7056b96ddee041f5698bb373eaee9b9ae56b
alesblaznik <ales@blaznik.si>
2013-05-23 20:58:19 +0200

    Minimizacija in razbitje JS knjižnic.

--- opencollea/assets.py
+++ opencollea/assets.py
@@ -4,23 +4,23 @@ from django_assets import Bundle, register
 css = Bundle('css/bootstrap.css',
              'css/bootstrap-responsive.css',
              'css/opencollea.css',
-             filters='cssmin', output='css/opencollea.css')
+             filters='cssmin', output='gen/opencollea.css')
 register('css_all', css)
 
 # JavaScript
-js = Bundle('js/jquery-2.0.0.js',
-            'js/bootstrap.js',
-            'js/angular.js',
+js = Bundle(#'js/jquery-2.0.0.js',
+            #'js/bootstrap.js',
+            #'js/angular.js',
             #'js/angular_resource.js',
             'js/angular-resource.js',
             'js/http-auth-interceptor.js',
             'js/services.js',
             'js/app.js',
             'js/controllers.js',
-            'js/raphael.js',
+            'js/raphael-min.js',
             'js/pie.js',
             'js/etherpad.js',
-            'js/ui-bootstrap-tpls-0.3.0.js',
+            'js/ui-bootstrap-tpls-0.3.0.min.js',
             'js/services/md5.js',
             'js/services/profile.js',
             'js/services/course.js',
@@ -33,5 +33,5 @@ js = Bundle('js/jquery-2.0.0.js',
             'js/controllers/home.js',
             'js/controllers/discover.js',
             'js/controllers/chat.js',
-            output='js/opencollea.js')
+            output='gen/opencollea.js')
 register('js_all', js)
